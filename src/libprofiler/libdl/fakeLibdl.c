33c6a0c0ea3b0e57c0789ae36d0963330c731072
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2012-10-15 00:08:25 +0400

    _dl_sym is used

--- src/libprofiler/libdl/fakeLibdl.c
+++ src/libprofiler/libdl/fakeLibdl.c
@@ -1,23 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <execinfo.h>
+#include <string.h>
 //#include "origLibdl.h"
 //#include "true_libdl.h"
 
+void* getReturnAddress(){ 
+	void * returnAddr = NULL;
+        int j, nptrs;
+        #define SIZE 100
+        void *buffer[100];
+        char **strings;
 
+        nptrs = backtrace(buffer, SIZE);
+        //printf("backtrace() returned %d addresses\n", nptrs);
 
-/*extern void *dlopen (__const char *__file, int __mode) 
+       //          The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
+       //              would produce similar output to the following: 
+
+        strings = backtrace_symbols(buffer, nptrs);
+        if (strings == NULL) {
+            perror("backtrace_symbols");
+	    return returnAddr; 
+        }
+
+	char * addr;
+
+	printf("%s\n", strings[2]);
+	addr =  strchr ( strings[2], '[' ); 	
+	unsigned int addr_i;
+	sscanf(addr,"[0x%x]",&addr_i);
+
+	printf("addr = \"%s\", return addr = %#x, points to = %p\n", addr, addr_i, (void*)addr_i);
+	//returnAddr = (void*)0x804873a;
+	returnAddr = (void*)addr_i;
+	printf("pointer = %p\n", returnAddr);
+	
+        free(strings);
+	return returnAddr;
+}
+
+void *dlsym (void *__restrict _handle,
+                    __const char *__restrict _name)
 {
-//	puts("Hello from fake libdl");
-	system("touch /tmp/11212121");
-//	return _dlopen(__file,__mode);
-	return NULL;
-
-}*/
-
-
-void *dlsym (void *__restrict __handle,
-                    __const char *__restrict __name)
-{
-      	puts("Hello from fake libdl");
-	return d1sym(__handle, __name);
+      	printf("Hello from fake dlsym(%p,%s)",_handle,_name);
+//	void * callerPointer = __builtin_return_address (0); 
+//	unsigned int caller = &callerPointer;
+//	printf("Caller = %x, this = %x\n", caller, &dlsym );
+	void * caller = getReturnAddress();
+	void * result = NULL; 
+	asm volatile(
+		"movl %1,%%eax;"
+		"movl %2,%%edx;"
+		"movl %3,%%ecx;"
+		"call _dl_sym;"
+		: "=r"(result)
+		: "r"(_handle),"r"(_name),"r"(caller)
+		: "eax", "edx", "ecx"
+	);
+	printf("after _dl_sym call = %p", result);
+	//void * result = (void *)_dl_sym(__handle, __name, caller);
+//	void * result = (void *)_dl_sym(_handle, _name, caller);
+	return result;
 }
 
--- src/libprofiler/test/test_dynamic_load/test_dynamic_load_app.c
+++ src/libprofiler/test/test_dynamic_load/test_dynamic_load_app.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-#include  <dlfcn.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <execinfo.h>
+#include <dlfcn.h>
 #define LIBTEST_LIB_PATH "libs/libtest_lib.so"
 
 
@@ -13,19 +16,50 @@ void (*testFunction4)(void);
 int main(){
 	void * handle = dlopen(LIBTEST_LIB_PATH, RTLD_LAZY); 
 	
+/*	void * callerPointer = __builtin_extract_return_addr(__builtin_frame_address(0)); 
+	unsigned int caller = &callerPointer;
+	printf("Caller_main = %x\n",  callerPointer );
+	printf("main = %x\n", &main);
+
+
+           int j, nptrs;
+       #define SIZE 100
+           void *buffer[100];
+           char **strings;
+
+           nptrs = backtrace(buffer, SIZE);
+           printf("backtrace() returned %d addresses\n", nptrs);
+
+  //          The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
+//              would produce similar output to the following: 
+
+           strings = backtrace_symbols(buffer, nptrs);
+           if (strings == NULL) {
+               perror("backtrace_symbols");
+               exit(EXIT_FAILURE);
+           }
+
+           for (j = 0; j < nptrs; j++)
+               printf("%s\n", strings[j]);
+
+           free(strings);*/
+
+
+
 	testFunction1 = dlsym(handle, "testFunction1");
-	testFunction2 = dlsym(handle, "testFunction2");
-	testFunction3 = dlsym(handle, "testFunction3");
-	testFunction4 = dlsym(handle, "testFunction4");
-	
+	//testFunction2 = dlsym(handle, "testFunction2");
+	//testFunction3 = dlsym(handle, "testFunction3");
+	//testFunction4 = dlsym(handle, "testFunction4");
+	printf("\ntestFunction1 = %p\n", testFunction1);	
+
 	printf("Calling testFunction1(): %d\n", (*testFunction1)(1,1));
-	printf("Calling testFunction2(): %d\n", (*testFunction2)());
+//	printf("Calling testFunction2(): %d\n", (*testFunction2)());
 
-	printf("Calling testFunction3()\n");
-	(*testFunction3)(1,1);
+//	printf("Calling testFunction3()\n");
+//	(*testFunction3)(1,1);
 
-	printf("Calling testFunction4()\n");
-	(*testFunction4)();
+//	printf("Calling testFunction4()\n");
+//	(*testFunction4)();
 
 	dlclose(handle);	
 	return 0;
