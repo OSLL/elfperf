5ef5e8fe878f6c859813d44fdd08f85dd499afa2
mark.zaslavskiy@gmail.com <root@debian.(none)>
2012-11-05 07:41:46 -0500

    #3581: added file for glibc build without prefix

---
+++ scripts/prepare_build_eglibc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script makes preparations for eglibc build
+
+rm -rf build
+mkdir -p build/manual
+touch build/manual/stamp.{o,op,os,OS}
+cd build 
+../eglibc-2.11.3/configure --enable-shared  --disable-sanity-checks
+
