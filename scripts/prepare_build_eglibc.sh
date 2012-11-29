b8e24604f0573bb57d909e9f3e2f1d3b122bdbd9
Edward Ryabikov <edward.ryabikov@gmail.com>
2012-11-29 22:24:18 +0400

    fixed prepare_build_eglibc script

--- scripts/prepare_build_eglibc.sh
+++ scripts/prepare_build_eglibc.sh
@@ -6,5 +6,5 @@ rm -rf build
 mkdir -p build/manual
 touch build/manual/stamp.{o,op,os,OS}
 cd build 
-../eglibc-2.11.3/configure --enable-shared  --disable-sanity-checks
+../../eglibc-2.11.3/configure --enable-shared  --disable-sanity-checks
 
