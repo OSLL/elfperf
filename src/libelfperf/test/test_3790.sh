8dcee0aa4c5453103dec4ee0c3972c6cce85b2bc
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2013-01-20 19:40:33 +0400

    #3748: added script for test running

--- src/libelfperf/test/test_3790.sh
+++ src/libelfperf/test/test_3790.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash -c 'LD_LIBRARY_PATH=/lib:/usr/lib:. LD_PRELOAD=../../src/libelfperf/libelfperf.so ELFPERF_ENABLE=true ELFPERF_PROFILE_FUNCTION=paaa:ddd /tools/lib/ld-2.11.3.so /bin/ls'
+bash -c 'LD_LIBRARY_PATH=/lib:/usr/lib:. LD_PRELOAD=../../src/libelfperf/libelfperf.so ELFPERF_ENABLE=true ELFPERF_PROFILE_FUNCTION=paaa:ddd /tools/lib/ld-2.11.3.so /bin/ls' 2>/dev/null 1>/dev/null
 
 if [ $? -eq 0 ] 
 then
--- src/libelfperf/test/test_3791.sh
+++ src/libelfperf/test/test_3791.sh
@@ -2,7 +2,7 @@
 
 
 
-bash -c 'LD_LIBRARY_PATH=/lib:/usr/lib:. LD_PRELOAD=../libelfperf.so ELFPERF_ENABLE=true /tools/lib/ld-2.11.3.so /bin/ls'
+bash -c 'LD_LIBRARY_PATH=/lib:/usr/lib:. LD_PRELOAD=../libelfperf.so ELFPERF_ENABLE=true /tools/lib/ld-2.11.3.so /bin/ls' 2>/dev/null 1>/dev/null
 
 if [ $? -eq 0 ] 
 then
