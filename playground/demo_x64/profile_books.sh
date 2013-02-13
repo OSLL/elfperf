ae79f5b246269a7ff7a2220b4a91ca4f00ba9964
Edward Ryabikov <edward.ryabikov@gmail.com>
2013-02-13 22:18:50 +0400

    added test 64-bit apps

---
+++ playground/demo_x64/profile_books.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bash -c 'LD_LIBRARY_PATH=/lib:/usr/lib:. LD_PRELOAD=../../src/libelfperf/libelfperf.so:/tools/lib/libdl.so.2 ELFPERF_ENABLE=true \
+ELFPERF_PROFILE_FUNCTION=\
+fclose\
+:fwrite /tools/lib/ld-2.11.3.so ./books'
+
