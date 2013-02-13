6c445bb555a9afb2ca0c820126be90f80c0bb46a
Edward Ryabikov <edward.ryabikov@gmail.com>
2013-02-13 23:27:11 +0400

    #3895: Added test for corrupting of real numbers by libdl.so

--- src/libelfperf/test/test_c_program.c
+++ src/libelfperf/test/test_c_program.c
@@ -2,15 +2,23 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <dlfcn.h>
 
 #include "libtest_c.h"
 
 int main()
 {
-    float resultFn2 = testFunction2(TEST_FUNCTION_2_ARG0, TEST_FUNCTION_2_ARG1);
-    printf("Result of testFunction2 = %f\n", resultFn2);
+//    float resultFn2 = testFunction2(TEST_FUNCTION_2_ARG0, TEST_FUNCTION_2_ARG1);
+//    printf("Result of testFunction2 = %f\n", resultFn2);
 
+    void* p = dlopen("./libtest_c.so", RTLD_LAZY);
+
+    void* f = dlsym(p, "testFunction2");
+    float(*fn)(int, double) = (float(*)(int, double))f;
+
+    float resultFn2 = fn(TEST_FUNCTION_2_ARG0, TEST_FUNCTION_2_ARG1);
     assert(fabs(resultFn2 - TEST_FUNCTION_2_RESULT) <= EPS);
+    printf("Result of testFunction2 = %f\n", resultFn2);
 
     return 0;
 }
